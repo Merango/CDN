64f501fef873afc5c8820b59676a151e17062199
Merango <Merango@users.noreply.github.com>
2025-06-25 15:10:32 +0000

    Add CommonJS and ES Module support for error-response module

--- src/utils/error-response.js
+++ src/utils/error-response.js
@@ -70,7 +70,15 @@ function createErrorLog(type, message, context = {}) {
   };
 }
 
+// CommonJS export
 module.exports = {
+  createErrorResponse,
+  createErrorLog,
+  ERROR_TYPES
+};
+
+// ES Module export (for TypeScript/modern environments)
+export {
   createErrorResponse,
   createErrorLog,
   ERROR_TYPES
